aa842536f3b72ce6fe899b9a6e536da00abf1ce0
Paul Buckley <paul@ubiqanalytics.com>
2023-01-04 19:16:29 +0000

    added sample code

---
+++ TestArea/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Newtonsoft.Json;
+using Ubiq.Extensions.Newtonsoft;
+using Ubiq.FourcastersAPI;
+using Ubiq.Http;
+
+HttpClient httpClient = HttpUtil.CreateHttpClient(false, false, false);
+var httpClientHelper = new HttpClientHelper(NullLogger<HttpClientHelper>.Instance, new JsonSerializerSettings().Configure());
+
+string username = "supernovatest";
+string password = "test469";
+
+var fourcasters = new FourcastersAPI(NullLogger<FourcastersAPI>.Instance, httpClientHelper, httpClient, new Uri("https://api.4casters.io/"), username, password, "USD", 1.0m);
+
+fourcasters.OrdersUpdated += Fourcasters_OrdersUpdated;
+fourcasters.PositionUpdated += Fourcasters_PositionUpdated;
+
+void Fourcasters_PositionUpdated(object sender, PositionUpdateMessage[] e)
+{
+    Console.WriteLine(JsonConvert.SerializeObject(e, Formatting.Indented));
+}
+
+void Fourcasters_OrdersUpdated(object sender, OrderUpdateMessage[] e)
+{
+    Console.WriteLine(JsonConvert.SerializeObject(e, Formatting.Indented));
+}
+
+await fourcasters.Login();
+await fourcasters.InitialiseWebSockets();
+
+CancelAllOrdersResponse cancelAllResponse = await fourcasters.CancelAllOrders();
+
+GamesResponse games = await fourcasters.GetGames("NHL");
+Game game = games.data.games.First();
+
+PlaceResponse placeResponse = await fourcasters.Place(new[] { new PlaceOrder()
+    {
+        gameID = game.id,
+        odds = +500m,
+        bet = 10m,
+        side = game.participants.First().id,
+        type = "moneyline",
+    }
+});
+
+CancelResponse cancelResponse = await fourcasters.Cancel(placeResponse.data.createdSessions.First().unmatched.OfferId);
+
+Console.ReadLine();
